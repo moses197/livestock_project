906f002f73c7f7591f0ff359e2c687354e2fbbf8
moses197 <leomoszeko@gmail.com>
2023-09-29 13:41:08 +0100

    added yield and sections

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-</head>
-<body>
+@extends('layout')
+
+@section('content')
     <section class="flex">
         <nav>
             <div class="nav-col">
@@ -38,5 +32,5 @@
             </div>
         </article>
     </section>
-</body>
-</html>
+
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-    
+    @yield('content')
 </body>
 </html>
