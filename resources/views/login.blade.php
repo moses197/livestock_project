c94ede5521a2fdf1e936143fd1b82e0121867f98
moses197 <leomoszeko@gmail.com>
2023-10-04 15:35:10 +0100

    tydidi

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,15 +9,16 @@
 <div class="form-content">
   <div><p style="font-size: xx-large; font-weight: bold;">Login Page</p></div>
   <form class="form-class">
-    <div>
-      <label for="exampleInputEmail1" class="form-label">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    </div>
-    <div>
-      <label for="exampleInputPassword1" class="form-label">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+      <div>
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      </div>
+      <div>
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+        <div><a href="" style="text-decoration: none;">Forget Password</a></div>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
   </form> 
 </div>
 
